74fd28d3eb764db1c09e5f08a340c2ff0b1c3db0
Sasha Fomina <Sasha Fomina>
2017-03-09 20:33:58 -0500

    expanded

--- QuickSort.java
+++ QuickSort.java
@@ -93,9 +93,9 @@ public class QuickSort
 	//System.out.println("upper:" + uppr);
 	//System.out.println("=============");
 	if(lower < uppr) {
-	    int pivotPos = partition(d, lower, uppr, uppr);
-	    qsortH(d, lower, pivotPos - 1);
-	    qsortH(d, pivotPos + 1, uppr);
+	    int pivotPos = partition(d, lower, uppr,lower);
+	    qsortH1(d, lower, pivotPos - 1);
+	    qsortH1(d, pivotPos + 1, uppr);
 	}
     }
     
@@ -112,8 +112,8 @@ public class QuickSort
 	//System.out.println("=============");
 	if(lower < uppr) {
 	    int pivotPos = partition(d, lower, uppr, (lower+uppr)/2);
-	    qsortH(d, lower, pivotPos - 1);
-	    qsortH(d, pivotPos + 1, uppr);
+	    qsortH2(d, lower, pivotPos - 1);
+	    qsortH2(d, pivotPos + 1, uppr);
 	}
     }
     
@@ -127,71 +127,8 @@ public class QuickSort
     //main method for testing
     public static void main( String[] args ) 
     {
-	//int[] arg = {5,4,17,9000,6};
-	///System.out.println("position:" + partition(arg, 0, 4, 2));
-	//printArr(arg);
-	    
-
-	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
-	//get-it-up-and-running, static test case:
-	int [] arr1 = {7,1,5,12,3};
-	System.out.println("\narr1 init'd to: " );
-	printArr(arr1);
-
-	qsort( arr1 );	
-       	System.out.println("arr1 after qsort: " );
-	printArr(arr1);
-
-	// randomly-generated arrays of n distinct vals
-	int[] arrN = new int[10];
-	for( int i = 0; i < arrN.length; i++ )
-	    arrN[i] = i;
-       
-	System.out.println("\narrN init'd to: " );
-	printArr(arrN);
-
-       	shuffle(arrN);
-       	System.out.println("arrN post-shuffle: " );
-	printArr(arrN);
-
-	qsort( arrN );
-	System.out.println("arrN after sort: " );
-	printArr(arrN);
-
-
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-
 	
-
-	//get-it-up-and-running, static test case w/ dupes:
-	int [] arr2 = {7,1,5,12,3,7};
-	System.out.println("\narr2 init'd to: " );
-	printArr(arr2);
-
-	qsort( arr2 );	
-       	System.out.println("arr2 after qsort: " );
-	printArr(arr2);
-
-
-	// arrays of randomly generated ints
-	int[] arrMatey = new int[20];
-	for( int i = 0; i < arrMatey.length; i++ )
-	    arrMatey[i] = (int)( 48 * Math.random() );
-       
-	System.out.println("\narrMatey init'd to: " );
-	printArr(arrMatey);
-
-       	shuffle(arrMatey);
-       	System.out.println("arrMatey post-shuffle: " );
-	printArr(arrMatey);
-
-	qsort( arrMatey );
-	System.out.println("arrMatey after sort: " );
-	printArr(arrMatey);
-	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	
+     
     }//end main
 
 }//end class QuickSort
--- QuickSortTester.java
+++ QuickSortTester.java
@@ -29,40 +29,47 @@ public class QuickSortTester
      Creates 10 arrays of 1,10,50,100,and 1000 items and finds the avg time in miliseconds it takes to complete a merge sort, printing out a trace diagram for each test case.
      ******************************/
 
-    public static long time(int[] arr){
-	MergeSort runner = new MergeSort();
-	long time = System.nanoTime();
-	runner.sort(arr);
-	time = System.nanoTime() - time;
-	return time;
+    public static int[] reverseArr(int[] arr){
+	int counter = 1;
+	for (int i = arr.length-1 ; i >=0; i--){
+	    arr[i] = counter;
+	    counter ++;
+	}
+	return arr;
     }
     
     public static void testArr(int[] arr, double time, int type){
-		System.out.println("~~~~~~~~~~ARRAY n = " + length + "~~~~~~~~~~~~~~");
+	double startTime ;
+	double endTime;
+
+	System.out.println("~~~~~~~~~~ARRAY n = " + arr.length + "~~~~~~~~~~~~~~");
 	int counter = 0;
 	while (counter<10){
 	    //populate a length sized array
-	    int x = 0;
-	    while(x<arr.length){
-		arr[x] = (int) (Math.random() * 50);
-		x++;
+	    if (type == 0 || type == 1){
+		int x = 0;
+		while(x<arr.length){
+		    arr[x] = (int) (Math.random() * 50);
+		    x++;
+		}
 	    }
-	    System.out.println("Testing on array ");
-	    QuickSort.printArr(arr);
-	    System.out.print(" ....\n");
+	    //System.out.println("Testing on array ");
+	    //QuickSort.printArr(arr);
+	    //System.out.print(" ....\n");
 
 	    //start stopwatch
-	    a = System.currentTimeMillis();
-	    if (type == 0){
+	    startTime = System.currentTimeMillis();
+	    System.out.println(startTime);
+	    if (type == 0 || type == 2){
 	    QuickSort.qsort1(arr);
 	    }
 	    else if (type == 1){
 		QuickSort.qsort2(arr);
 	    }
-	    b= System.currentTimeMillis();
-
-	    System.out.println("Time: " + (b - a));
-	    time += b-a;
+	    endTime= System.currentTimeMillis();
+	    System.out.println(endTime);
+	    time += endTime-startTime;
+	    System.out.println("Time: " + (endTime-startTime));
 	    counter ++;
 	}
 
@@ -77,8 +84,6 @@ public class QuickSortTester
 	int[] test4000 = new int[4000];
 	int[] test3000 = new int[3000];
 	
-	double a = 0;
-	double b = 0;
 	double time1000 = 0;
 	double time5000 = 0;
 	double time2000 = 0;
@@ -86,11 +91,15 @@ public class QuickSortTester
 	double time3000 = 0;
 	double time4000 = 0;
       
-	//WORST CASE PIVOT POS
-	testArr(test1000, time1000, 
+	//AVG CASE
+	testArr(test1000, time1000, 0);
+	testArr(test1500, time1500, 0);
+	testArr(test2000, time2000, 0);	
+	testArr(test3000, time3000, 0);
+	testArr(test4000, time4000, 0);	
+	testArr(test5000, time5000, 0);	
 	
-	
-	System.out.println("~~~~~~~~~~~~~~WORST PIVOT POS~~~~~~~~~~~~~~~~~~");
+	System.out.println("~~~~~~~~~~~~~~AVG CASE~~~~~~~~~~~~~~~~~~");
 
 	
 	System.out.println("AVG TIME FOR n=1000:" + (time1000 *1.0 / 10));
@@ -98,14 +107,68 @@ public class QuickSortTester
 	System.out.println("AVG TIME FOR n=2000:" + (time2000 *1.0 / 10));
 	System.out.println("AVG TIME FOR n=1500:" + (time1500 *1.0 / 10));
 	System.out.println("AVG TIME FOR n=3000:" + (time3000 *1.0 / 10));
-	System.out.println("AVG TIME FOR n=4000:" + (time4000 *1.0 / 10));		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	System.out.println("AVG TIME FOR n=4000:" + (time4000 *1.0 / 10));
+	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 	/*
-	System.out.println("Standard nlogn time when n is 9");
-	System.out.println("Time merge sort takes to sort array of length 9");
-	System.out.println("\n\n\n");
-	System.out.println("Unsorted array len 9 : " + time(test1));
-	System.out.println("Sorted array len 9 : " + time(test2));
-	*/
+	//BEST CASE
+	time1000 = 0;
+	time5000 = 0;
+	time2000 = 0;
+	time1500 = 0;
+	time3000 = 0;
+	time4000 = 0;      
+	
+	testArr(test1000, time1000, 1);
+	testArr(test1500, time1500, 1);
+	testArr(test2000, time2000, 1);	
+	testArr(test3000, time3000, 1);
+	testArr(test4000, time4000, 1);	
+	testArr(test5000, time5000, 1);	
+	
+	System.out.println("~~~~~~~~~~~~~~BEST CASE~~~~~~~~~~~~~~~~~~");
+
+	
+	System.out.println("AVG TIME FOR n=1000:" + (time1000 *1.0 / 10));
+	System.out.println("AVG TIME FOR n=5000:" + (time5000 *1.0 / 10));		
+	System.out.println("AVG TIME FOR n=2000:" + (time2000 *1.0 / 10));
+	System.out.println("AVG TIME FOR n=1500:" + (time1500 *1.0 / 10));
+	System.out.println("AVG TIME FOR n=3000:" + (time3000 *1.0 / 10));
+	System.out.println("AVG TIME FOR n=4000:" + (time4000 *1.0 / 10));		
+	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+	//WORST CASE
+	time1000 = 0;
+	time5000 = 0;
+	time2000 = 0;
+	time1500 = 0;
+	time3000 = 0;
+	time4000 = 0;
+	
+	test1000 = reverseArr(test1000);
+	test1500 = reverseArr(test1500);
+	test2000 = reverseArr(test2000);
+	test5000 = reverseArr(test5000);
+	test4000 = reverseArr(test4000);
+	test3000 = reverseArr(test3000);
+
+	testArr(test1000, time1000, 2);
+	testArr(test1500, time1500, 2);
+	testArr(test2000, time2000, 2);	
+	testArr(test3000, time3000, 2);
+	testArr(test4000, time4000, 2);	
+	testArr(test5000, time5000, 2);
+
+	System.out.println("~~~~~~~~~~~~~~WORST CASE~~~~~~~~~~~~~~~~~~");
+
+	
+	System.out.println("AVG TIME FOR n=1000:" + (time1000 *1.0 / 10));
+	System.out.println("AVG TIME FOR n=5000:" + (time5000 *1.0 / 10));		
+	System.out.println("AVG TIME FOR n=2000:" + (time2000 *1.0 / 10));
+	System.out.println("AVG TIME FOR n=1500:" + (time1500 *1.0 / 10));
+	System.out.println("AVG TIME FOR n=3000:" + (time3000 *1.0 / 10));
+	System.out.println("AVG TIME FOR n=4000:" + (time4000 *1.0 / 10));
+	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        */
     }//end main
 
 }//end class
